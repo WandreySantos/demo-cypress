1716ea58803e374638b113c7df7a1a4f10d2a4b0
WandreySantos <wandreymiceoni16@gmail.com>
2026-05-17 20:23:48 -0300

    adiciona testes end-to-end com cypress

--- cypress/cypress/integration/spec1.js
+++ cypress/cypress/integration/spec1.js
@@ -1,5 +1,27 @@
-describe('Meu primeiro teste', () => {
-   it('Não faz nada', () => {
-     expect(true).to.equal(true)
-   })
-})
+describe("Teste End-to-End", () => {
+  it("Teste completo", () => {
+    cy.visit("http://localhost:5000/");
+
+    cy.get("[data-id=3]").should("contain.text", "Design Patterns");
+
+    cy.get("[data-id=3]").within(() => {
+      cy.get("input").type("10000-000");
+      cy.contains("Calcular Frete").click();
+      cy.wait(2000);
+    });
+
+    cy.get(".swal-text").contains("O frete é: R$");
+
+    cy.get(".swal-button").click();
+
+    cy.get("[data-id=3]").within(() => {
+      cy.contains("Comprar").click();
+    });
+
+    cy.wait(2000);
+
+    cy.get(".swal-text").contains("Sua compra foi realizada com sucesso");
+
+    cy.get(".swal-button").click();
+  });
+});
